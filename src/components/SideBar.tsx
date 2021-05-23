018fde69792995c36d9642c285d072d930f4ebff
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2021-05-23 12:39:44 -0300

    Finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { Genre } from '../models/genre';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Genre[]
+  handleChangeGenre?: (genre: Genre) => void;
+  selectedGenge: Genre;
+}
+
+export function SideBar({ genres, handleChangeGenre, selectedGenge }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleChangeGenre && handleChangeGenre(genre)}
+            selected={selectedGenge.id === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
